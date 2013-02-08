2788516aa67bd09aa300942aea13d6eeddf88396
Rodman Henley <rodmanhe@hotmail.com>
2013-02-08 13:34:21 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Outpouring::Application.config.secret_token = '5b2f77bbb230b8e84f99bf2ea2534c0ccf5e68a41e5e15885d8f403dd7e2814e56a1ce4baaa7b7a8f26537af594e325abae53b0eda00cf2d99fd33ba66e6c282'
