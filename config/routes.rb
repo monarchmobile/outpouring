ec9d1b79acf77e437e4bd7fbf84c72bdee350158
Andrew Gross <monarchmobile@gmail.com>
2013-02-21 09:17:12 -0500

    tried to make local identical to origin

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Outpouring::Application.routes.draw do
 
   
 
+  
+
   mount RailsAdmin::Engine => '/admin', :as => 'rails_admin'
 
   devise_for :users, path_names: {sign_in: "login", sign_out: "logout"}
