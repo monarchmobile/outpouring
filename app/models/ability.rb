5185528d3264d920bc590c9ad9a261668d7cf7f7
Rodman Henley <rodmanhe@hotmail.com>
2013-02-20 22:50:14 -0500

    added permission to non admins to prevent users from signing up as admin

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -1,14 +1,18 @@
 class Ability
   include CanCan::Ability
-  
   def initialize(user)
-    user ||= User.new # guest user
-    
-    if user.role? :admin
-      can :manage, :all
-    else
-      can :read, :all
+    can :read, :all 				  # allow everyone to read everything
+    user ||= User.new 				  # guest user              
+    if user && user.admin?
+      can :access, :rails_admin       # only allow admin users to access Rails Admin
+      can :dashboard                  # allow access to dashboard
+      if user.role? :superadmin
+        can :manage, :all             # allow superadmins to do anything
+      elsif user.role? :manager
+        can :manage, [User, Product]  # allow managers to do anything to products and users
+      elsif user.role? :sales
+        can :update, Product, :hidden => false  # allow sales to only update visible products
+      end
     end
-      
   end
 end
