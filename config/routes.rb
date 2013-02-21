0b3d37309a853618239455e935a63afdb871e581
Rodman Henley <rodmanhe@hotmail.com>
2013-02-21 09:45:52 -0500

    added drop_rails_admin_histories migration

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Outpouring::Application.routes.draw do
 
   
 
+  
+
   mount RailsAdmin::Engine => '/admin', :as => 'rails_admin'
 
   devise_for :users, path_names: {sign_in: "login", sign_out: "logout"}
---
+++ db/migrate/20130221144222_drop_table_rails_admin_histories.rb
@@ -0,0 +1,8 @@
+class DropTableRailsAdminHistories < ActiveRecord::Migration
+  def up
+  	drop_table :rails_admin_histories
+  end
+
+  def down
+  end
+end
