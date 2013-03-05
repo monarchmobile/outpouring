a8bbd956f0cf5e28069766a36270c0a723f7c15c
Rodman Henley <rodmanhe@hotmail.com>
2013-03-05 17:25:25 -0500

    refactored comment form and added to home page

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,6 +1,9 @@
 class StaticPagesController < ApplicationController
 	
   def home
+  	# @article = Article.find(1)
+  	@article = Article.find(1)
+    @comment = @article.comments.build
   end
 
   def about
