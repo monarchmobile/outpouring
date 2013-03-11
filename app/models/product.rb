df1c31327f8ee433627f91e0cc262eec56e69499
Rodman Henley <rodmanhe@hotmail.com>
2013-03-11 09:37:35 -0400

    deleted line about accepts_nested_attributes :piggybak_sellable

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,7 +1,7 @@
 class Product < ActiveRecord::Base
   attr_accessible :description, :slug, :title, :piggybak_sellable_attributes
 
-  accepts_nested_attributes_for :piggybak_sellable
+ 
   
   acts_as_sellable_with_variants
   # acts_as_sellable
