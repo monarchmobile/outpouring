9c99983b375f4822accca36a66d5b7c25c7668e1
Andrew Gross <monarchmobile@gmail.com>
2013-03-23 02:25:45 -0400

    changed css extention

--- config/application.rb
+++ config/application.rb
@@ -73,7 +73,7 @@ module Outpouring
 
     config.assets.initialize_on_precompile = false
     config.action_mailer.default_url_options = { host: ENV["MAILER_HOST"] }
-    config.assets.precompile << "static_pages.css.scss"
+    config.assets.precompile << "static_pages.css"
 
     config.autoload_paths += %W(#{config.root}/app/models/ckeditor)
   end
