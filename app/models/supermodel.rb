c32408c9b01f31d6f88c468b80f9528e4c3b2c46
Rodman Henley <rodmanhe@hotmail.com>
2013-04-23 17:02:48 -0400

    added visisble method to supermodels

--- app/models/supermodel.rb
+++ app/models/supermodel.rb
@@ -1,3 +1,12 @@
-class Supermodel < ActiveRecord::Base 
-  attr_accessible :name, :visible
+class Supermodel < ActiveRecord::Base
+	attr_accessible :name, :visible, :position
+
+	def self.visible
+		where(visible: true)
+	end
+
+	def self.hidden
+		where(visible: false)
+	end
+
 end
